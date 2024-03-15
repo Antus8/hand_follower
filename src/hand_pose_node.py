6bb6b98993ffc800741cf41a0bf414edeac7d91f
Antus8 <antus.longo@gmail.com>
2024-03-15 11:25:12 +0100

    Track hand position wrt image center

--- src/hand_pose_node.py
+++ src/hand_pose_node.py
@@ -2,7 +2,7 @@
 
 import cv2
 import rospy
-import numpy as numpy
+import numpy as np
 import mediapipe as mp
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -12,9 +12,12 @@ from sensor_msgs.msg import Image
 class HandDetector:
     def __init__(self):
         self.br = CvBridge()
+        self.out_pub = rospy.Publisher("/bebop_ws/out_image", Image, queue_size=10)
         self.sub = rospy.Subscriber("/bebop_ws/camera_image", Image, self.image_callback)
         self.mp_detector = mp.solutions.hands
         self.hand_detector = self.mp_detector.Hands(min_detection_confidence=0.75, min_tracking_confidence=0.5)
+
+        self.image_size = None
 
         self.mp_draw = mp.solutions.drawing_utils
 
@@ -22,46 +25,54 @@ class HandDetector:
     def image_callback(self, msg):
         rospy.loginfo("I received an image")
         frame = self.br.imgmsg_to_cv2(msg)
+
+        self.image_size = [frame.shape[1], frame.shape[0]]
+
+        flipped_frame = cv2.flip(frame, 1)
         
-        with self.hand_detector as hands:
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            rgb_frame.flags.writeable = False
-            result = hands.process(rgb_frame)
-            # rgb_frame.flags.writeable = True
-            print(result.multi_handedness)
-            if result.multi_hand_landmarks:
-                for num, hand in enumerate(result.multi_hand_landmarks):
-                    print(hand)
-                    wrist_lmk = hand.landmark[self.hand_detector.HandLandmark.WRIST]
-                    self.mp_draw.draw_landmarks(frame, hand, mp.hands.HAND_CONNECTIONS,
-                                                self.mp_draw.DrawingSpecs(color=(121,22,76), thickness=2, circle_radius=4),
-                                                self.mp_draw.DrawingSpecs(color=(121,44,250), thickness=2, circle_radius=2))
-                    
-                    get_label(num, hand, result)
+        rgb_frame = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2RGB)
+        rgb_frame.flags.writeable = False
+        result = self.hand_detector.process(rgb_frame)
 
-                    cv2.imshow("final img", frame)
-                    if cv2.waitKey(1) == ord('q'):
-                        break
-
-
-    def get_label(index, hands, results):
-        output = None
-        for idx, classification in enumerate(results.multi_handedness):
-            if classification.classification[0].index = index:
-                label = classification.classification[0].label
-                text = str(label)
-
-                coords = tuple(np.multiply(
-                    np.array((hand.landmark[self.hand_detector.HandLandmark.WRIST].x, hand.landmark[self.hand_detector.HandLandmark.WRIST].y)),
-                    [640,480], astype(int)))
+        image = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
+        # rgb_frame.flags.writeable = True
+        if result.multi_hand_landmarks:
+            for num, hand in enumerate(result.multi_hand_landmarks):
+                self.mp_draw.draw_landmarks(image, hand, self.mp_detector.HAND_CONNECTIONS,
+                                            self.mp_draw.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=2),
+                                            self.mp_draw.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
                 
-                output = text, coords
+                out = self.get_hand_center(num, hand, result)
+                if out:
+                    hand_center_x = out[0]
+                    hand_center_y = out[1]
+                    cv2.circle(image, (hand_center_x, hand_center_y), 3, (0, 255, 0), 2)
+                    cv2.circle(image, (int(self.image_size[0]/2), int(self.image_size[1]/2)), 3, (0, 255, 0), 2) # place a circle in the center of the image
 
-        return output
+
+                    cv2.line(image, (hand_center_x, hand_center_y), (int(self.image_size[0]/2), int(self.image_size[1]/2)), (0,255,0), 2)
+
+                self.out_pub.publish(self.br.cv2_to_imgmsg(cv2.flip(image, 1)))
+
+
+    def get_hand_center(self, index, hands, results):
+        # output = None
+        for idx, classification in enumerate(results.multi_handedness):
+            if classification.classification[0].index == index:
+                # label = classification.classification[0].label
+                # text = str(label)
+                coords = tuple(np.multiply(
+                    np.array((hands.landmark[self.mp_detector.HandLandmark.MIDDLE_FINGER_MCP].x, hands.landmark[self.mp_detector.HandLandmark.MIDDLE_FINGER_MCP].y)),
+                    self.image_size).astype(int))
+                
+                # output = [text, coords]
+
+                return coords
+        return None
 
 def main():
-    rospy.init_node("subscriber_node")
-    my_node = Subscriber()
+    rospy.init_node("hand_detector_node")
+    my_node = HandDetector()
     while not rospy.is_shutdown():
         rospy.spin()
 
